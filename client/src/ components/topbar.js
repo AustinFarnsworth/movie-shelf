a8fe725956a879d81dbe257f6ce2b1b0ca2e6135
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2021-12-22 15:57:47 -0700

    app structure

---
+++ client/src/ components/topbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./topbar.css";
+
+function Topbar() {
+  return (
+    <div className="topbar-container">
+      <div>Welcome to Movie Shelf</div>
+    </div>
+  );
+}
+
+export default Topbar;
