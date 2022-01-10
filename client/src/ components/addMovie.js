da861455f9aea7e62964e254408e1d6dcd2e079b
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2022-01-10 16:27:15 -0700

    add movie function finished

--- client/src/ components/addMovie.js
+++ client/src/ components/addMovie.js
@@ -1,13 +1,37 @@
-import React, {useState} from "react";
+import React, {useState, useContext} from "react";
+import {MovieContext} from "../context/movieContext";
 
 function AddMovie() {
   const [name, setName] = useState("");
   const [rating, setRating] = useState("");
+  const [movies, setMovies] = useContext(MovieContext);
+
+  const updateName = (e) => {
+    setName(e.target.value);
+  };
+
+  const updateRating = (e) => {
+    setRating(e.target.value);
+  };
+
+  const addMovie = (e) => {
+    e.preventDefault();
+    setMovies((prevMovies) => [...prevMovies, {name: name, rating: rating}]);
+  };
 
   return (
     <form>
-      <input type="text" placeholder="name" value={name} />
-      <input type="text" placeholder="rating" value={rating} />
+      <input type="text" name="name" value={name} onChange={updateName} />
+      <input
+        type="text"
+        placeholder="rating"
+        name="rating"
+        value={rating}
+        onChange={updateRating}
+      />
+      <button type="submit" onClick={addMovie}>
+        Add Movie
+      </button>
     </form>
   );
 }
