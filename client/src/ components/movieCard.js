514b8b0ccb4d35bed04b2126f92b83b814cc4279
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2022-01-25 15:12:21 -0700

    card template finished

--- client/src/ components/movieCard.js
+++ client/src/ components/movieCard.js
@@ -20,6 +20,33 @@ const MovieCard = ({name, rating}) => {
             <li>Adventure, Drama</li>
           </ul>
         </div>
+        <br />
+        <div className="summary-row">
+          <div className="colum2">
+            <h5>Summary</h5>
+          </div>
+          {/* <div className="column2">
+          <div>Movie Likes</div>
+        </div> */}
+        </div>
+        <div className="column1">
+          <p className="movie-summary">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
+        <div>
+          <div className="column1">
+            <p className="movie-actors">
+              Chris Evans, Hugo Weaving, Hayley Atwell
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
