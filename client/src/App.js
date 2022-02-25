8bb82fd26cade84b879fe4af571bb01e0ecec251
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2022-02-25 15:08:05 -0700

    git movies api call

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,27 @@ import "./App.css";
 import Topbar from "./ components/topbar";
 import MoviesList from "./ components/moviesList";
 import {MovieProvider} from "./context/movieContext";
+import {useState} from "react";
 import AddMovie from "./ components/addMovie";
+import movieDatabase from "./api/movieDatabase";
 
 function App() {
+  const [movie, setMovie] = useState("");
+
+  const getMovie = () => {
+    movieDatabase.get("/601").then((response) => {
+      // console.log(response.data);
+      setMovie(response.data.title);
+    });
+  };
+
   return (
     <MovieProvider>
       <div className="App">
         <Topbar />
         <AddMovie />
+        <button onClick={getMovie}>Get Movies</button>
+        <p>{movie}</p>
         <MoviesList />
       </div>
     </MovieProvider>
--- client/src/api/movieDatabase.js
+++ client/src/api/movieDatabase.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-require("dotenv").config();
+// require("dotenv").config();
 
-const KEY = process.env.REACT_API_KEY;
+// const KEY = process.env.REACT_API_KEY;
+const KEY = "42fafab1d67d95a1993da7575e94ff60";
 
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3/movie",
