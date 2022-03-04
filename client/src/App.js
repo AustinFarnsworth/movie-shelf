efa76a91c7c6c0dbd7640e7847ca3ff7fdea8fd7
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2022-03-04 11:27:39 -0700

    useEffect hook added on get movies call

--- client/src/App.js
+++ client/src/App.js
@@ -2,26 +2,26 @@ import "./App.css";
 import Topbar from "./ components/topbar";
 import MoviesList from "./ components/moviesList";
 import {MovieProvider} from "./context/movieContext";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import AddMovie from "./ components/addMovie";
 import movieDatabase from "./api/movieDatabase";
 
 function App() {
   const [movie, setMovie] = useState("");
 
-  const getMovie = () => {
-    movieDatabase.get("/601").then((response) => {
-      // console.log(response.data);
+  useEffect(() => {
+    movieDatabase.get("/557").then((response) => {
+      console.log(response.data);
       setMovie(response.data.title);
     });
-  };
+  });
 
   return (
     <MovieProvider>
       <div className="App">
         <Topbar />
         <AddMovie />
-        <button onClick={getMovie}>Get Movies</button>
+        {/* <button>Get Movies</button> */}
         <p>{movie}</p>
         <MoviesList />
       </div>
