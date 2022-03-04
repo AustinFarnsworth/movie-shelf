5b79d3bc9436d1437f162a75ef1b3d49aad226ba
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2022-03-04 11:42:57 -0700

    axios call changed to movieCard component

--- client/src/ components/movieCard.js
+++ client/src/ components/movieCard.js
@@ -1,49 +1,56 @@
-import React, {useContext} from "react";
+import React, {useState, useEffect} from "react";
 import {MovieContext} from "../context/movieContext";
 import "./movieCard.css";
+import movieDatabase from "../api/movieDatabase";
 
 const MovieCard = () => {
-  const [movies, setMovies] = useContext(MovieContext);
+  const [movies, setMovies] = useState("");
+  const [genres, setGenres] = useState("");
+
+  useEffect(() => {
+    movieDatabase
+      .get("/557")
+      .then((response) => {
+        console.log(response.data.genres);
+        setMovies(response.data);
+        setGenres(response.data.genres);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div>
-      {movies.map((movie) => {
-        return (
-          <div className="card-container">
-            <div className="movie" key={movie.id}>
-              <img
-                className="movie-img"
-                src="../images/movietheatre.jpg"
-                alt="movie"
-              />
-            </div>
-            <div className="movie-text">
-              <div className="column1">
-                <h1>{movie.title}</h1>
-                <ul className="movie-desc">
-                  <li>{movie.movieRating}</li>
-                  <li>{movie.runtime}</li>
-                  <li>{movie.genre}</li>
-                </ul>
-              </div>
-              <br />
-              <div className="summary-row">
-                <div className="colum2">
-                  <h5>Summary</h5>
-                </div>
-              </div>
-              <div className="column1">
-                <p className="movie-summary">{movie.movieSummary}</p>
-              </div>
-              <div>
-                <div className="column1">
-                  <p className="movie-actors">{movie.actors}</p>
-                </div>
-              </div>
+      <div className="card-container">
+        <div className="movie" key={movies.id}>
+          <img className="movie-img" src={movies.poster_path} alt="movie" />
+        </div>
+        <div className="movie-text">
+          <div className="column1">
+            <h1>{movies.title}</h1>
+            <ul className="movie-desc">
+              <li>{movies.movieRating}</li>
+              <li>{movies.runtime} mins</li>
+              {/* <li>{genres}</li> */}
+            </ul>
+          </div>
+          <br />
+          <div className="summary-row">
+            <div className="colum2">
+              <h5>{movies.overview}</h5>
             </div>
           </div>
-        );
-      })}
+          <div className="column1">
+            <p className="movie-summary">{movies.movieSummary}</p>
+          </div>
+          <div>
+            <div className="column1">
+              <p className="movie-actors">{movies.actors}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,16 @@ import MoviesList from "./ components/moviesList";
 import {MovieProvider} from "./context/movieContext";
 import {useEffect, useState} from "react";
 import AddMovie from "./ components/addMovie";
-import movieDatabase from "./api/movieDatabase";
 
 function App() {
   const [movie, setMovie] = useState("");
-
-  useEffect(() => {
-    movieDatabase.get("/557").then((response) => {
-      console.log(response.data);
-      setMovie(response.data.title);
-    });
-  });
 
   return (
     <MovieProvider>
       <div className="App">
         <Topbar />
         <AddMovie />
-        {/* <button>Get Movies</button> */}
-        <p>{movie}</p>
+
         <MoviesList />
       </div>
     </MovieProvider>
