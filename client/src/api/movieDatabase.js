15134a3100d4fa4c20787b9a6cdc0874f9aa894f
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2022-03-08 10:10:34 -0700

    .env keys added

--- client/src/api/movieDatabase.js
+++ client/src/api/movieDatabase.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-// require("dotenv").config();
-
-// const KEY = process.env.REACT_API_KEY;
-const KEY = "42fafab1d67d95a1993da7575e94ff60";
+const KEY = process.env.REACT_APP_API_KEY;
 
 export default axios.create({
   baseURL: "https://api.themoviedb.org/3/movie",
